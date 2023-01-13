a1c2ab6a0969492adda8abe618aff41048fe91b9
pauljrowland <17676570+pauljrowland@users.noreply.github.com>
2023-01-13 14:52:41 +0000

    v1.2a - Win2016-2019 Converter Upload

---
+++ Win2016-2019Workstation/Converter/Start Here.cmd
@@ -0,0 +1,21 @@
+cls
+@ECHO OFF
+@ECHO.
+@ECHO WindowsWorkstation.com
+@ECHO Convert your Windows Server 2016 into a Desktop Now!
+@ECHO.
+@ECHO v1.2a
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO Please wait, starting Windows PowerShell
+@ECHO this may take a few moments...
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+powershell.exe -File "%~dp0Scripts\Wrapper.ps1"
+Exit
